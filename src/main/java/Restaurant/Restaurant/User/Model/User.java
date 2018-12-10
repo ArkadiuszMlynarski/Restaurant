428753f00c1a5d43963f5005c9c79c2c25f2c2e3
Mateusz Partyka <mateuszpartyka12@gmail.com>
2018-12-10 14:41:21 +0100

    add Role class

--- src/main/java/Restaurant/Restaurant/User/Model/User.java
+++ src/main/java/Restaurant/Restaurant/User/Model/User.java
@@ -2,9 +2,10 @@ package Restaurant.Restaurant.User.Model;
 
 import lombok.Data;
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.*;
 
-@Data
+
 @Entity
 @Table(name = "Users")
 public class User implements Serializable {
@@ -17,4 +18,65 @@ public class User implements Serializable {
     private String username;
     private String password;
 
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Role> roles;
+
+    public User(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
 }
