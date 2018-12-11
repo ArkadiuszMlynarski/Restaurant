b9bf0a43b351d1cebc4a77aa58ff0515569cb10e
Damian Olech <unova4@gmail.com>
2018-12-11 11:20:38 +0100

    Restaurant service and repository update

--- src/main/java/Restaurant/Restaurant/Restaurant/service/RestaurantServiceImpl.java
+++ src/main/java/Restaurant/Restaurant/Restaurant/service/RestaurantServiceImpl.java
@@ -1,4 +1,27 @@
 package Restaurant.Restaurant.Restaurant.service;
 
+import Restaurant.Restaurant.Restaurant.Model.Restaurant;
+import Restaurant.Restaurant.Restaurant.repository.RestaurantRepository;
+
+import java.util.List;
+
 public class RestaurantServiceImpl implements RestaurantService {
+
+    @Autowired
+    RestaurantRepository repository;
+
+    @Override
+    public void addRestaurant(Restaurant restaurant) {
+
+    }
+
+    @Override
+    public List<Restaurant> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Restaurant getByname(String name) {
+        return repository.findByName(name);
+    }
 }
