fccdeecba0e5ea46dde0c75f22d44d5cd67640fe
Mateusz Partyka <mateuszpartyka12@gmail.com>
2018-12-11 17:57:27 +0100

    add homepages

--- src/main/java/Restaurant/Restaurant/User/Controller/UserController.java
+++ src/main/java/Restaurant/Restaurant/User/Controller/UserController.java
@@ -32,9 +32,14 @@ public class UserController {
         userService.addUser(user);
     }
 
-    @GetMapping("test")
-    public String test(){
-        return "test";
+    @GetMapping("/user/homepage")
+    public String userHomePage(){
+        return "userHomePage";
+    }
+
+    @GetMapping("/admin/homepage")
+    public String adminHomePage(){
+        return "userHomePage";
     }
 
 
