89e0bda79492c822286645672d58c699fd9a7369
niemiecs <szymon19965@gmail.com>
2018-12-11 12:52:11 +0100

    add dish repository

---
+++ src/main/java/Restaurant/Restaurant/Dish/repository/DishRepository.java
@@ -0,0 +1,9 @@
+package Restaurant.Restaurant.Dish.repository;
+
+import Restaurant.Restaurant.Dish.Model.Dish;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DishRepository extends JpaRepository<Dish,Long> {
+}
