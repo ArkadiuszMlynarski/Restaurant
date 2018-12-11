a102f9f2d796c9329ef5bfd4a14178afc908ba40
Damian Olech <unova4@gmail.com>
2018-12-11 13:19:25 +0100

    add method to resaurant controllert

--- src/main/java/Restaurant/Restaurant/Restaurant/Controller/RestaurantController.java
+++ src/main/java/Restaurant/Restaurant/Restaurant/Controller/RestaurantController.java
@@ -5,6 +5,8 @@ import Restaurant.Restaurant.Restaurant.service.RestaurantServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/restaurant")
 public class RestaurantController {
@@ -12,11 +14,14 @@ public class RestaurantController {
     @Autowired
     RestaurantServiceImpl restaurantService;
 
-
-
+    @GetMapping("/getAll")
+    public List<Restaurant> getAll() {
+        return restaurantService.getAll();
+    }
 
 
     @PostMapping("/add")
-    public void addRestaurant(@RequestBody Restaurant restaurant)
-    {restaurantService.addRestaurant(restaurant);}
+    public void addRestaurant(@RequestBody Restaurant restaurant) {
+        restaurantService.addRestaurant(restaurant);
+    }
 }
