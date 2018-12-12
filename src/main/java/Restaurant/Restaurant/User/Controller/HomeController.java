686e763b0f14d843f2850e22727d8fff42956316
Mateusz Partyka <mateuszpartyka12@gmail.com>
2018-12-12 11:44:16 +0100

    remove homeController, add index.html

--- src/main/java/Restaurant/Restaurant/User/Controller/HomeController.java
+++
@@ -1,9 +0,0 @@
-package Restaurant.Restaurant.User.Controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/")
-public class HomeController {
-}
