b86c8bada9d66d9444a665052479834ce97843e0
Mateusz Partyka <mateuszpartyka12@gmail.com>
2018-12-14 16:00:51 +0100

    add getUser test

--- src/test/java/Restaurant/Restaurant/User/service/UserServiceImplTest.java
+++ src/test/java/Restaurant/Restaurant/User/service/UserServiceImplTest.java
@@ -1,5 +1,41 @@
+package Restaurant.Restaurant.User.service;
+
+import Restaurant.Restaurant.User.Model.User;
+import Restaurant.Restaurant.User.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import static org.junit.Assert.*;
 
 public class UserServiceImplTest {
 
+    UserService userService;
+
+    @Before
+    public void setUp() throws Exception {
+        userService = Mockito.mock(UserService.class);
+    }
+
+    @Test
+    public void addUser() {
+        User user = Mockito.mock(User.class);
+
+        userService.addUser(user);
+        userService.isUserExist(user);
+    }
+
+    @Test
+    public void getAll() {
+
+
+
+    }
+
+    @Test
+    public void getByUsername() {
+    }
 }
