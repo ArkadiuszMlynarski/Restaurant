9daa83fc9c897b2747fdbed40110006599b51909
Mateusz Partyka <mateuszpartyka12@gmail.com>
2018-12-14 15:59:53 +0100

    add currentUser in homepage

--- src/main/java/Restaurant/Restaurant/User/Controller/UserController.java
+++ src/main/java/Restaurant/Restaurant/User/Controller/UserController.java
@@ -5,8 +5,12 @@ import Restaurant.Restaurant.User.Model.User;
 import Restaurant.Restaurant.User.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
@@ -22,7 +26,14 @@ public class UserController {
     UserServiceImpl userService;
 
     @GetMapping("/homepage")
-    public String userHomePage(){
+    public String userHomePage(Model model){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            model.addAttribute("currentUserName", currentUserName);
+        }
+
         return "homepage";
     }
 
