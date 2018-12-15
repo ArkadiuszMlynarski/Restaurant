b31e6d15d72fa7c18529fe4621cef32884f81b41
Mateusz Partyka <mateuszpartyka12@gmail.com>
2018-12-15 16:42:32 +0100

    update Order model

--- src/main/java/Restaurant/Restaurant/Dish/Model/Dish.java
+++ src/main/java/Restaurant/Restaurant/Dish/Model/Dish.java
@@ -1,23 +1,28 @@
 package Restaurant.Restaurant.Dish.Model;
 
 
+import Restaurant.Restaurant.Order.Model.OrderModel;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 @Data
 @Entity
-@Table(name = "Dishes")
+@Table(name = "dish")
 public class Dish {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
-
     private float price;
+
+    @ManyToMany(mappedBy = "dishes")
+    private List<OrderModel> orders;
+
 
     public Dish() {
 
