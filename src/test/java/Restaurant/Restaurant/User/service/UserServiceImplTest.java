54fb893e65fb560ddc310a2909aa4f244adb5ee4
Mateusz Partyka <mateuszpartyka12@gmail.com>
2018-12-15 16:42:46 +0100

    create test getByUsername

--- src/test/java/Restaurant/Restaurant/User/service/UserServiceImplTest.java
+++ src/test/java/Restaurant/Restaurant/User/service/UserServiceImplTest.java
@@ -9,6 +9,8 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Optional;
+
 import static org.junit.Assert.*;
 
 public class UserServiceImplTest {
@@ -23,7 +25,6 @@ public class UserServiceImplTest {
     @Test
     public void addUser() {
         User user = Mockito.mock(User.class);
-
         userService.addUser(user);
         userService.isUserExist(user);
     }
@@ -31,11 +32,27 @@ public class UserServiceImplTest {
     @Test
     public void getAll() {
 
-
-
     }
 
     @Test
     public void getByUsername() {
+        User user = new User();
+        user.setUsername("testowy");
+        userService.addUser(user);
+        Optional<User> getUser = userService.getByUsername("testowy");
+
+        if(getUser.isPresent()) {
+            System.out.println(getUser.get().getUsername());
+        }
+    }
+
+    @Test
+    public void getById() {
+
+    }
+
+
+    @Test
+    public void isUserExist() {
     }
 }
