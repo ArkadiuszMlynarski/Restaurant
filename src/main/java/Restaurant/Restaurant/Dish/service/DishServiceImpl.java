310d9cdc17e659633ea672b1569dc78d7faa0c01
arkadiuszmlynarski <piromysl@gmail.com>
2018-12-16 17:55:00 +0100

    add edit, remove dish methods

--- src/main/java/Restaurant/Restaurant/Dish/service/DishServiceImpl.java
+++ src/main/java/Restaurant/Restaurant/Dish/service/DishServiceImpl.java
@@ -6,6 +6,7 @@ import Restaurant.Restaurant.Dish.repository.DishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DishServiceImpl implements Dishservice {
@@ -27,5 +28,21 @@ public class DishServiceImpl implements Dishservice {
     @Override
     public Dish getByName(String dishname) {
         return repository.findByName(dishname);
+    }
+
+    @Override
+    public Optional<Dish> getById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public void editDish(Dish dish) {
+        repository.deleteById(dish.getId());
+        repository.save(dish);
+    }
+
+    @Override
+    public void removeDish(Long id) {
+        repository.deleteById(id);
     }
 }
