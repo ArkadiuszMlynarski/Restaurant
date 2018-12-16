c66070aee38cadb66b237b08101333cf9743ce0b
arkadiuszmlynarski <piromysl@gmail.com>
2018-12-16 17:41:32 +0100

    add edit, remove restaurant methods

--- src/main/java/Restaurant/Restaurant/Restaurant/Controller/RestaurantController.java
+++ src/main/java/Restaurant/Restaurant/Restaurant/Controller/RestaurantController.java
@@ -2,8 +2,11 @@ package Restaurant.Restaurant.Restaurant.Controller;
 
 import Restaurant.Restaurant.Restaurant.Model.Restaurant;
 import Restaurant.Restaurant.Restaurant.service.RestaurantServiceImpl;
+import Restaurant.Restaurant.User.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/restaurant")
@@ -19,4 +22,35 @@ public class RestaurantController {
     @PostMapping("/add")
     public void addRestaurant(@RequestBody Restaurant restaurant)
     {restaurantService.addRestaurant(restaurant);}
+
+
+    @PostMapping("/editRestaurant")
+    public void editRestaurant(@RequestBody Restaurant restaurant) {
+        restaurantService.editRestaurant(restaurant);
+    }
+
+    @GetMapping("/editRestaurantById={id}")
+    public void editRestaurant(@PathVariable Long id) {
+        Optional<Restaurant> tempOptRestaurant = restaurantService.getById(id);
+
+        if(tempOptRestaurant.isPresent()){
+            restaurantService.editRestaurant(tempOptRestaurant.get());
+        }
+    }
+
+    @PostMapping("/removeRestaurant")
+    public void removeRestaurant(@RequestBody Restaurant restaurant){
+        restaurantService.removeRestaurant(restaurant.getId());
+    }
+
+    @GetMapping("removeRestaurantById={id}")
+    public void removeRestaurant(@PathVariable Long id){
+        Optional<Restaurant> tempOptRestaurant = restaurantService.getById(id);
+
+        if(tempOptRestaurant.isPresent()){
+            restaurantService.editRestaurant(tempOptRestaurant.get());
+        }
+        restaurantService.removeRestaurant(id);
+
+    }
 }
--- src/main/java/Restaurant/Restaurant/Restaurant/service/RestaurantService.java
+++ src/main/java/Restaurant/Restaurant/Restaurant/service/RestaurantService.java
@@ -3,6 +3,7 @@ package Restaurant.Restaurant.Restaurant.service;
 import Restaurant.Restaurant.Restaurant.Model.Restaurant;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface RestaurantService {
@@ -12,4 +13,10 @@ public interface RestaurantService {
     public List<Restaurant> getAll();
 
     public Restaurant getByname(String name);
+
+    public void editRestaurant(Restaurant restaurant);
+
+    public void removeRestaurant(Long id);
+
+    public Optional<Restaurant> getById(Long id);
 }
