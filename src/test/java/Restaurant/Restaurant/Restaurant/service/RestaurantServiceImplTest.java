8b8ca7379af67aba407d492e91af2a4f4a65373e
Mateusz Partyka <mateuszpartyka12@gmail.com>
2018-12-18 20:30:20 +0100

    fix bug

--- src/test/java/Restaurant/Restaurant/Restaurant/service/RestaurantServiceImplTest.java
+++ src/test/java/Restaurant/Restaurant/Restaurant/service/RestaurantServiceImplTest.java
@@ -9,12 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -41,7 +45,6 @@ public class RestaurantServiceImplTest {
     public void getAll() {
         when(restaurantRepository.findAll()).thenReturn(Stream
                 .of(new Restaurant("mcDolan","ul.Morska 99"), new Restaurant("KFC","ul.Ziemana 88")).collect(Collectors.toList()));
-        System.out.println(restaurantRepository);
         assertEquals(2, restaurantRepository.findAll().size());
     }
 
@@ -69,16 +72,16 @@ public class RestaurantServiceImplTest {
 
         restaurant1.setName(restToUpdate.getName());
         restaurant1.setAddress(restToUpdate.getAddress());
-        restaurantRepository.save(restaurant1);
-
-        assertEquals(restaurant1.getName(),restToUpdate.getName());
+        when(restaurantRepository.save(restaurant1)).thenReturn(restaurant1);
+        assertEquals(restaurantRepository.save(restaurant1).getName(),restToUpdate.getName());
     }
 
     @Test
     public void removeRestaurant() {
+        Long id = new Long(1);
+        restaurantService.removeRestaurant(id);
+        verify(restaurantRepository, times(1)).deleteById(id);
     }
 
-    @Test
-    public void isNameUsed() {
-    }
+   
 }
