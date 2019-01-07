e14fdc84c26eeda814fd337944ac851fb2bb7822
Mateusz Partyka <mateuszpartyka12@gmail.com>
2019-01-07 14:54:31 +0100

    periodic report Controller

--- src/main/java/Restaurant/Restaurant/DailyReport/Controller/PeriodicReportController.java
+++ src/main/java/Restaurant/Restaurant/DailyReport/Controller/PeriodicReportController.java
@@ -1,7 +1,32 @@
 package Restaurant.Restaurant.DailyReport.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
+@RequestMapping("admin/report")
 public class PeriodicReportController {
+
+    @GetMapping("/periodicReportPage")
+    public String getPeriodicReportHomepage(){
+
+        return "report/periodicReportHomepage";
+    }
+
+    @PostMapping("/getReportByDate")
+    public String getReportByDate(@RequestParam String begin, @RequestParam String end){
+
+        LocalDate localDateBegin = LocalDate.parse(begin);
+        LocalDate localDateEnd = LocalDate.parse(end);
+
+        System.out.println("LocalDate od: = "+localDateBegin+",    do: "+localDateEnd);
+        return "report/periodicReportHomepage";
+    }
+
+
 }
